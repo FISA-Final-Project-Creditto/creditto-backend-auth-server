e16ec2c07eb5f1d3f11669afecb09d1c5483e290
Jeyong <dlwpdyd201@naver.com>
2025-11-06 16:04:27 +0900

    feat: JWT 토큰 생성 및 검증을 위한 RSA 키쌍 설정

---
+++ src/main/java/org/creditto/authserver/auth/jwt/RsaKeyUtil.java
@@ -0,0 +1,50 @@
+package org.creditto.authserver.auth.jwt;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.io.Resource;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+import java.security.KeyFactory;
+import java.security.NoSuchAlgorithmException;
+import java.security.interfaces.RSAPrivateKey;
+import java.security.interfaces.RSAPublicKey;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.PKCS8EncodedKeySpec;
+import java.security.spec.X509EncodedKeySpec;
+import java.util.Base64;
+
+import static org.creditto.authserver.auth.constants.AlgorithmConstants.RSA;
+import static org.creditto.authserver.auth.constants.Constants.*;
+
+@Component
+@RequiredArgsConstructor
+public class RsaKeyUtil {
+
+    private final RsaKeyProperties rsaKeyProperties;
+
+    public RSAPublicKey getPublicKey() throws IOException, NoSuchAlgorithmException, InvalidKeySpecException {
+        KeyFactory keyFactory = KeyFactory.getInstance(RSA);
+        byte[] decodedKey = getDecodedKeyBytes(rsaKeyProperties.publicKeyPath(), PUBLIC_KEY_BEGIN, PUBLIC_KEY_END);
+        X509EncodedKeySpec keySpec = new X509EncodedKeySpec(decodedKey);
+        return (RSAPublicKey) keyFactory.generatePublic(keySpec);
+    }
+
+    public RSAPrivateKey getPrivateKey() throws IOException, InvalidKeySpecException, NoSuchAlgorithmException {
+        KeyFactory keyFactory = KeyFactory.getInstance(RSA);
+        byte[] decodedKey = getDecodedKeyBytes(rsaKeyProperties.privateKeyPath(), PRIVATE_KEY_BEGIN, PRIVATE_KEY_END);
+        PKCS8EncodedKeySpec keySpec = new PKCS8EncodedKeySpec(decodedKey);
+        return (RSAPrivateKey) keyFactory.generatePrivate(keySpec);
+    }
+
+    private byte[] getDecodedKeyBytes(Resource keyResource, String beginMarker, String endMarker) throws IOException {
+        String key = new String(keyResource.getInputStream().readAllBytes());
+
+        String pem = key
+                .replace(beginMarker, "")
+                .replaceAll(System.lineSeparator(), "")
+                .replace(endMarker, "");
+
+        return Base64.getDecoder().decode(pem);
+    }
+}
