3ecb046065ffffeb2d64f573a5d7da35ada87d66
Jeyong <dlwpdyd201@naver.com>
2025-11-06 10:37:13 +0900

    test: 임시 테스트 코드

--- src/test/java/org/creditto/authserver/AuthServerApplicationTests.java
+++ src/test/java/org/creditto/authserver/AuthServerApplicationTests.java
@@ -1,7 +1,12 @@
 package org.creditto.authserver;
 
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class AuthServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
 }
